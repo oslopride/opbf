f3a79e27518d5b86ce779095834cc61197048ccf
camorte <carloalfredo.morte@gmail.com>
2021-11-06 18:01:02 +0100

    add initial schema

---
+++ oslopridebusinessforum/schemas/page.js
@@ -0,0 +1,14 @@
+export default {
+  title: "Index",
+  type: "document",
+  name: "page",
+  fields: [
+    { title: "Hero banner image", name: "heroImage", type: "image" },
+    { title: "Hero banner text", name: "heroText", type: "string" },
+    {
+      title: "Hero banner description",
+      name: "heroDescription",
+      type: "string",
+    },
+  ],
+};
