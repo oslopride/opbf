c57e26bd1a49661cf455c25c07f0b4334e2b2f3a
camorte <carloalfredo.morte@gmail.com>
2021-11-14 13:07:56 +0100

    update footer with links and sections

--- opbf-site/src/components/footer.tsx
+++ opbf-site/src/components/footer.tsx
@@ -1,13 +1,19 @@
 /** @jsxImportSource @emotion/react */
 
 import WhiteLogo from "../assets/logo-white.svg";
+import FbLogo from "../assets/facebook.svg";
+import InstaLogo from "../assets/insta.svg";
 import { css } from "@emotion/react";
 import theme from "../utils/theme";
 import styled from "@emotion/styled";
 
-type Props = {};
+type Props = {
+	fbLink?: string;
+	instaLink?: string;
+	linkedInLink?: string;
+};
 
-const Footer: React.FC<Props> = () => {
+const Footer: React.FC<Props> = ({ fbLink, instaLink, linkedInLink }) => {
 	return (
 		<div
 			css={css`
@@ -16,7 +22,41 @@ const Footer: React.FC<Props> = () => {
 			`}
 		>
 			<FooterWrapper>
-				<OsloPrideLogo src={`${WhiteLogo}`} alt="Oslo Pride logo" />
+				<Logos>
+					<OsloPrideLogo src={`${WhiteLogo}`} alt="Oslo Pride logo" />
+					<div
+						css={css`
+							display: flex;
+							flex-direction: row;
+						`}
+					>
+						{fbLink && (
+							<a href={fbLink}>
+								<SocialLogo src={`${FbLogo}`} />
+							</a>
+						)}
+						{instaLink && (
+							<a href={instaLink}>
+								<SocialLogo src={`${InstaLogo}`} />{" "}
+							</a>
+						)}
+					</div>
+				</Logos>
+				<div
+					css={css`
+						display: flex;
+						@media screen and (max-width: 720px) {
+							flex-direction: column;
+						}
+					`}
+				>
+					<Address>
+						<h1>Hello</h1>
+					</Address>
+					<Shortcuts>
+						<h1>Bye</h1>
+					</Shortcuts>
+				</div>
 			</FooterWrapper>
 		</div>
 	);
@@ -25,14 +65,17 @@ const Footer: React.FC<Props> = () => {
 export default Footer;
 
 const FooterWrapper = styled.div`
-	display: grid;
-	grid-template-columns: 3fr 2fr 1fr;
-	column-gap: 1rem;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
 
 	padding: 2.5rem 1rem;
 	width: 90vw;
-	max-width: 900px;
 	margin: 0 auto;
+
+	@media screen and (max-width: 720px) {
+		flex-direction: column;
+	}
 `;
 
 const OsloPrideLogo = styled.img`
@@ -40,3 +83,17 @@ const OsloPrideLogo = styled.img`
 	fill: ${theme.color.text.white};
 	margin-left: -1rem;
 `;
+
+const SocialLogo = styled.img`
+	width: 1.5rem;
+	fill: ${theme.color.text.white};
+	margin-right: 0.5rem;
+`;
+
+const Logos = styled.div`
+	display: flex;
+	flex-direction: column;
+`;
+
+const Address = styled.div``;
+const Shortcuts = styled.div``;
