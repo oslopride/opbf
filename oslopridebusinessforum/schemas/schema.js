231c91a663ba666d1a646234971bd39024a791b7
camorte <carloalfredo.morte@gmail.com>
2021-11-14 13:13:07 +0100

    add new program schema

--- oslopridebusinessforum/schemas/schema.js
+++ oslopridebusinessforum/schemas/schema.js
@@ -2,8 +2,9 @@ import createSchema from "part:@sanity/base/schema-creator";
 
 import schemaTypes from "all:part:@sanity/base/schema-type";
 import mainpage from "./mainpage";
+import program from "./program";
 
 export default createSchema({
   name: "default",
-  types: schemaTypes.concat([mainpage]),
+  types: schemaTypes.concat([mainpage, program]),
 });
