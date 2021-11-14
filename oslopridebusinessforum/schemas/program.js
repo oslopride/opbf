bedc2cf12d64e827d45d03d834a9625ff7e750d1
camorte <carloalfredo.morte@gmail.com>
2021-11-14 14:08:39 +0100

    add hero component to program schema

--- oslopridebusinessforum/schemas/program.js
+++ oslopridebusinessforum/schemas/program.js
@@ -3,7 +3,50 @@ export default {
   name: "program",
   type: "document",
   __experimental_actions: ["update", "publish"],
+  fieldsets: [
+    {
+      name: "hero",
+      title: "Hero banner",
+      options: {
+        collapsible: true,
+        collapsed: false,
+      },
+    },
+  ],
   fields: [
+    {
+      title: "Hero banner image",
+      name: "heroImage",
+      type: "image",
+      fieldset: "hero",
+    },
+    {
+      title: "Hero banner title",
+      name: "heroText",
+      type: "string",
+      fieldset: "hero",
+    },
+    {
+      title: "Hero banner description",
+      name: "heroDescription",
+      type: "string",
+      fieldset: "hero",
+    },
+    {
+      title: "Hero buttons",
+      name: "heroButtons",
+      type: "array",
+      of: [
+        {
+          type: "object",
+          fields: [
+            { title: "Button text", name: "buttonText", type: "string" },
+            { title: "Button URL", name: "buttonLink", type: "url" },
+          ],
+        },
+      ],
+      fieldset: "hero",
+    },
     {
       title: "Program content",
       name: "programContent",
